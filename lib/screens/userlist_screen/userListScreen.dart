904272976c25e709cc27f6db89dcf0f746d27cf2
nayeemx11 <nayeemx11@gmail.com>
2023-08-28 06:34:41 +0600

    refactoring

--- lib/screens/userlist_screen/userListScreen.dart
+++ lib/screens/userlist_screen/userListScreen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mediag/updateusersdetails.dart';
+import 'package:mediag/screens/updateuserdetails/updateusersdetails.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../authentication/login_screen.dart';
